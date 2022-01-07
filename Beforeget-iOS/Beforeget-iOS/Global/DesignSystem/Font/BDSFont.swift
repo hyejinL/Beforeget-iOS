93c67ebe0872efbcff1755573e54a743e6257424
so yeon <alicesysy@swu.ac.kr>
2022-01-07 18:33:13 +0900

    [Feat] 디자인 시스템(폰트) 구축 (#5)

---
+++ Beforeget-iOS/Beforeget-iOS/Global/DesignSystem/Font/BDSFont.swift
@@ -0,0 +1,31 @@
+//
+//  BDSFont.swift
+//  Beforeget-iOS
+//
+//  Created by soyeon on 2022/01/07.
+//
+
+import UIKit
+
+public enum BDSFont {
+    static let title1 = FontFamily.Pretendard.medium.font(size: 23)
+    static let title2 = FontFamily.Pretendard.regular.font(size: 23)
+    static let title3 = FontFamily.Pretendard.semiBold.font(size: 22)
+    static let title4 = FontFamily.Pretendard.semiBold.font(size: 19)
+    static let title5 = FontFamily.Pretendard.medium.font(size: 19)
+    static let title6 = FontFamily.Pretendard.semiBold.font(size: 17)
+    
+    static let body1 = FontFamily.Pretendard.regular.font(size: 17)
+    static let body2 = FontFamily.Pretendard.regular.font(size: 15)
+    static let body3 = FontFamily.Pretendard.semiBold.font(size: 14)
+    static let body4 = FontFamily.Pretendard.regular.font(size: 14)
+    
+    static let enHead1 = FontFamily.PPNeueMachina.regular.font(size: 22)
+    static let enBody1 = FontFamily.PPNeueMachina.ultrabold.font(size: 43)
+    static let enBody2 = FontFamily.PPNeueMachina.regular.font(size: 40)
+    static let enBody3 = FontFamily.PPNeueMachina.regular.font(size: 20)
+    static let enBody4 = FontFamily.PPNeueMachina.regular.font(size: 18)
+    static let enBody5 = FontFamily.PPNeueMachina.regular.font(size: 16)
+    static let enBody6 = FontFamily.PPNeueMachina.regular.font(size: 13)
+    static let enBody7 = FontFamily.PPNeueMachina.regular.font(size: 10)
+}
---
+++ Beforeget-iOS/Beforeget-iOS/Global/DesignSystem/UIComponet/Temp.swift
@@ -0,0 +1,8 @@
+//
+//  Temp.swift
+//  Beforeget-iOS
+//
+//  Created by soyeon on 2022/01/07.
+//
+
+import Foundation
