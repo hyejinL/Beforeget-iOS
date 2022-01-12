fafaf04f7d46669994f9ec5080b42a26b2eb3d9f
heerucan <iwpow0307@gmail.com>
2022-01-12 22:05:05 +0900

    [Add] Asset 및 Font 추가 (#4)

--- Beforeget-iOS/Beforeget-iOS/Global/DesignSystem/Font/BDSFont.swift
+++ Beforeget-iOS/Beforeget-iOS/Global/DesignSystem/Font/BDSFont.swift
@@ -14,6 +14,7 @@ public enum BDSFont {
     static let title4 = FontFamily.Pretendard.semiBold.font(size: 19)
     static let title5 = FontFamily.Pretendard.medium.font(size: 19)
     static let title6 = FontFamily.Pretendard.semiBold.font(size: 17)
+    static let title7 = FontFamily.Pretendard.medium.font(size: 17)
     
     static let body1 = FontFamily.Pretendard.regular.font(size: 17)
     static let body2 = FontFamily.Pretendard.semiBold.font(size: 16)
@@ -24,6 +25,7 @@ public enum BDSFont {
     static let body7 = FontFamily.Pretendard.semiBold.font(size: 14)
     static let body8 = FontFamily.Pretendard.regular.font(size: 14)
     static let body9 = FontFamily.Pretendard.medium.font(size: 13)
+    static let body10 = FontFamily.Pretendard.regular.font(size: 13)
     
     static let enHead1 = FontFamily.PPNeueMachina.ultrabold.font(size: 21)
     static let enHead2 = FontFamily.PPNeueMachina.regular.font(size: 20)
@@ -37,3 +39,5 @@ public enum BDSFont {
     static let enBody7 = FontFamily.PPNeueMachina.regular.font(size: 13)
     static let enBody8 = FontFamily.PPNeueMachina.regular.font(size: 10)
 }
+
+
