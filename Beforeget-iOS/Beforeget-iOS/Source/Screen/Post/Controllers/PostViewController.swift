a27334572dc7583cca2328a2f7e4a32dccae0737
eunseo5355 <eunseo5650525@gmail.com>
2022-01-13 19:08:57 +0900

    [Feat] Tool Bar, Header View 구현 (#10)

--- Beforeget-iOS/Beforeget-iOS/Source/Screen/Post/Controllers/PostViewController.swift
+++ Beforeget-iOS/Beforeget-iOS/Source/Screen/Post/Controllers/PostViewController.swift
@@ -7,23 +7,120 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 class PostViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    // MARK: - Properties
+    
+    private lazy var navigationBar = BDSNavigationBar(self, view: .write, isHidden: false)
+    
+    private let doneButton = UIButton().then {
+        $0.setTitle("완료", for: .normal)
+        $0.setTitleColor(Asset.Colors.gray200.color, for: .normal)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private lazy var writingTableView = UITableView().then {
+        $0.backgroundColor = Asset.Colors.white.color
+        $0.sectionHeaderTopPadding = 0
+        
+        $0.delegate = self
+        $0.dataSource = self
     }
-    */
+    
+    private lazy var toolBar = UIToolbar().then {
+        $0.barTintColor = Asset.Colors.white.color
+        $0.setItems([UIBarButtonItem(customView: addItemButton), toolBarSpacing, UIBarButtonItem(customView: deleteItemButton)], animated: true)
+    }
+    
+    private let toolBarSpacing = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil).then {
+        $0.width = 15
+    }
+    
+    private let addItemButton = UIButton().then {
+        var config = UIButton.Configuration.plain()
+        config.title = "기록항목 추가"
+        config.image = Asset.Assets.icnAddItem.image
+        config.imagePadding = 8
+        config.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        config.baseForegroundColor = Asset.Colors.black200.color
+        $0.configuration = config
+    }
+    
+    private let deleteItemButton = UIButton().then {
+        var config = UIButton.Configuration.plain()
+        config.title = "기록항목 삭제"
+        config.image = Asset.Assets.icnEdit.image
+        config.imagePadding = 4
+        config.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        config.baseForegroundColor = Asset.Colors.black200.color
+        $0.configuration = config
+    }
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configUI()
+        setupLayout()
+    }
+    
+    // MARK: - InitUI
+    
+    private func configUI() {
+        view.backgroundColor = .white
+    }
+    
+    private func setupLayout() {
+        navigationController?.setNavigationBarHidden(true, animated: true)
+        view.addSubviews([navigationBar,
+                          doneButton,
+                          writingTableView,
+                          toolBar])
+        
+        navigationBar.snp.makeConstraints {
+            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        doneButton.snp.makeConstraints {
+            $0.top.equalTo(navigationBar.snp.top).inset(6)
+            $0.trailing.equalTo(navigationBar.snp.trailing).inset(13)
+        }
+        
+        writingTableView.snp.makeConstraints {
+            $0.top.equalTo(navigationBar.snp.bottom)
+            $0.leading.bottom.trailing.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        toolBar.snp.makeConstraints {
+            $0.leading.bottom.trailing.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+    }
+    
+    // MARK: - Custom Method
 
 }
+
+extension PostViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension PostViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return WritingHeaderView()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 192
+    }
+}
+
+
---
+++ Beforeget-iOS/Beforeget-iOS/Source/Screen/Post/Views/Components/WritingHeaderView.swift
@@ -0,0 +1,139 @@
+//
+//  WritingHeaderView.swift
+//  Beforeget-iOS
+//
+//  Created by 배은서 on 2022/01/13.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class WritingHeaderView: UITableViewHeaderFooterView {
+    
+    static let identifier = "WritingHeaderView"
+    // MARK: - Properties
+    
+    private let dateButton = UIButton().then {
+        var config = UIButton.Configuration.plain()
+        config.title = "2022. 01. 08. SAT"
+        config.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer { incoming in
+            var outgoing = incoming
+            outgoing.font = BDSFont.enBody7
+            outgoing.foregroundColor = Asset.Colors.black200.color
+            return outgoing
+        }
+        config.contentInsets = NSDirectionalEdgeInsets(top: 9, leading: 18, bottom: 9, trailing: 18)
+        
+        $0.configuration = config
+        $0.layer.cornerRadius = 16
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = Asset.Colors.black200.color.cgColor
+    }
+    
+    private let star1 = UIButton().then {
+        $0.setImage(Asset.Assets.btnStarInactive.image, for: .normal)
+    }
+    
+    private let star2 = UIButton().then {
+        $0.setImage(Asset.Assets.btnStarInactive.image, for: .normal)
+    }
+    
+    private let star3 = UIButton().then {
+        $0.setImage(Asset.Assets.btnStarInactive.image, for: .normal)
+    }
+    
+    private let star4 = UIButton().then {
+        $0.setImage(Asset.Assets.btnStarInactive.image, for: .normal)
+    }
+    
+    private let star5 = UIButton().then {
+        $0.setImage(Asset.Assets.btnStarInactive.image, for: .normal)
+    }
+    
+    private let starDescriptionLabel = UILabel().then {
+        $0.text = "이 영화의 별점은 몇 점인가요?"
+        $0.font = BDSFont.body9
+        $0.textColor = Asset.Colors.gray300.color
+    }
+    
+    private let lineView = UIView().then {
+        // 색 고칠것!!!!!!!!! gray400
+        $0.backgroundColor = Asset.Colors.gray300.color
+    }
+    
+    
+    // MARK: - Life Cycle
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        configUI()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - InitUI
+    
+    private func configUI() {
+        backgroundColor = .white
+    }
+    
+    private func setupLayout() {
+        contentView.addSubviews([dateButton,
+                     star1,
+                     star2,
+                     star3,
+                     star4,
+                     star5,
+                     starDescriptionLabel,
+                     lineView])
+        
+        dateButton.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(20)
+            $0.centerX.equalToSuperview()
+        }
+        
+        star1.snp.makeConstraints {
+            $0.top.equalTo(dateButton.snp.bottom).offset(41)
+            $0.trailing.equalTo(star2.snp.leading)
+        }
+        
+        star2.snp.makeConstraints {
+            $0.top.equalTo(dateButton.snp.bottom).offset(31)
+            $0.trailing.equalTo(star3.snp.leading)
+        }
+        
+        star3.snp.makeConstraints {
+            $0.top.equalTo(star1.snp.top)
+            $0.centerX.equalToSuperview()
+        }
+        
+        star4.snp.makeConstraints {
+            $0.top.equalTo(star2.snp.top)
+            $0.leading.equalTo(star3.snp.trailing)
+        }
+        
+        star5.snp.makeConstraints {
+            $0.top.equalTo(star1.snp.top)
+            $0.leading.equalTo(star4.snp.trailing)
+        }
+        
+        starDescriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(star3.snp.bottom).offset(5)
+            $0.centerX.equalToSuperview()
+        }
+        
+        lineView.snp.makeConstraints {
+            $0.top.equalTo(starDescriptionLabel.snp.bottom).offset(33)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(1)
+        }
+    }
+    
+    // MARK: - Custom Method
+    
+}
