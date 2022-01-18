57576c96db8dcf4b2e2e0016faf1839419936ab0
so yeon <alicesysy@swu.ac.kr>
2022-01-19 01:39:04 +0900

    [Chore] BaseResponse 코드리뷰 반영 (#41)

--- Beforeget-iOS/Beforeget-iOS/Network/Base/BaseResponse.swift
+++ Beforeget-iOS/Beforeget-iOS/Network/Base/BaseResponse.swift
@@ -13,3 +13,25 @@ struct BaseResponse<T: Decodable>: Decodable {
     let message: String
     let data: T
 }
+
+struct BaseArrayResponseType<T: Decodable>: Decodable {
+    let status: Int
+    let success: Bool?
+    let message: String?
+    let data: [T]?
+    
+    enum CodingKeys: String, CodingKey {
+        case message
+        case data
+        case status
+        case success
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        message = (try? values.decode(String.self, forKey: .message)) ?? ""
+        data = (try? values.decode([T].self, forKey: .data)) ?? []
+        status = (try? values.decode(Int.self, forKey: .status)) ?? 0
+        success = (try? values.decode(Bool.self, forKey: .success)) ?? false
+    }
+}
