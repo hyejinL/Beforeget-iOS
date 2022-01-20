2e7d1ab61b881def4cdda4dfd3cdf7f70ccbb575
heerucan <iwpow0307@gmail.com>
2022-01-20 18:29:47 +0900

    [Chore] , 추가 (#45)

--- Beforeget-iOS/Beforeget-iOS/Source/Screen/MyRecord/Controllers/MyRecordViewController.swift
+++ Beforeget-iOS/Beforeget-iOS/Source/Screen/MyRecord/Controllers/MyRecordViewController.swift
@@ -190,7 +190,7 @@ extension MyRecordViewController:
         print(mediaString, "이거미디어어레이")
         
         let starArray = star.map { String($0) }
-        var starString = starArray.joined(separator: "")
+        var starString = starArray.joined(separator: ",")
         print(starString, "이거스타어레이")
 
         if media.isEmpty {
